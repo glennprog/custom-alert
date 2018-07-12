e6eb3f60679a9621387de343104aa116370996d0
Glenn <glenn.milingui@gmail.com>
2018-07-12 13:00:10 +0200

    custom tool: Alert. Exemple custom et bootstrap aussi. Css à part pour le rendu de la page avec un très beau design qui peut servir pour autre chose ;)

---
+++ alert-toolbox/gm-alert.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+    //console.log( "ready!" );
+    //console.log("application");
+
+    var close = document.getElementsByClassName("gm-closebtn");
+    var i;
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function(){
+            var div = this.parentElement;
+            div.style.opacity = "0";
+            setTimeout(function(){ div.style.display = "none"; }, 600);
+        }
+    }
+    
+});
